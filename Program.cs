16e58f59642d644da1bf239361b06249a3f5062d
Marshall <deeringmw@gmail.com>
2017-05-30 09:04:25 -0400

    Initial framework.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Recognition;
+
+namespace VoiceBotCons
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+           
+
+            Choices commands = new Choices();
+            commands.Add(new string[]
+            {
+              // "newcommand'
+              "dkp",
+              "test"
+            });
+
+            GrammarBuilder gBuilder = new GrammarBuilder();
+            gBuilder.Append(commands);
+            Grammar grammar = new Grammar(gBuilder);
+
+            //Load Grammar into recognition engine.
+            recEngine.LoadGrammarAsync(grammar);
+            //set microphone to default system mic.
+            recEngine.SetInputToDefaultAudioDevice();
+
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+
+            recEngine.SpeechRecognized += RecEngine_SpeechRecognized;
+            //  \/\/\/DO NOT DELETE THIS! MAKES THE WHOLE THING WORK, IDK\/\/\/\/
+            Console.Read();
+            //  /\/\/\/DO NOT DELETE THIS! MAKES THE WHOLE THING WORK, IDK\/\/\/\/\
+        }
+
+        public static void RecEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text)
+            {
+                case "dkp":
+                    Console.WriteLine("50 DKP Minus!");
+                    break;
+                case "test":
+                    Console.WriteLine("Printing Test");
+                    break;
+            }
+        }
+    }
+}
